387d39cf42c2583614313721ff7597124455127f
exitinfo <wngus_569@naver.com>
2020-05-23 14:44:24 +0000

    youtube

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/elliotforbes/youtube-stats
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/elliotforbes/youtube-stats/youtube"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
+}
+
+func stats(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	go websocket.Writer(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/stats", stats)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	fmt.Println("Youtube Subscriber Monitor")
+
+	//	item, err := youtube.GetSubscribers()
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//	fmt.Printf("%+v\n", item)
+	setupRoutes()
+}
